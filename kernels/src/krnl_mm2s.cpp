783ef00c95b75628efaf9eaeac24ae8bf6c426cd
Mario Ruiz <mruiznog@xilinx.com>
2020-07-03 15:04:57 +0100

    Add files to generate kernel xo

---
+++ kernels/src/krnl_mm2s.cpp
@@ -0,0 +1,87 @@
+/**********
+Copyright (c) 2019, Xilinx, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********/
+
+/* This is a data mover kernel which reads data from global memory(DDR) via
+memory mapped interface
+and writes to a stream interface to another kernel */
+
+#include "ap_axi_sdata.h"
+#include "ap_int.h"
+#include "hls_stream.h"
+
+#define DWIDTH 512
+#define UWIDTH 16
+
+typedef ap_axiu<DWIDTH, UWIDTH, 1, 1> pkt;
+
+
+extern "C" {
+void krnl_mm2s(ap_uint<DWIDTH>  *in,  // Read-Only Vector 1
+               hls::stream<pkt> &k2n, // Internal Stream
+               unsigned int     size, // Size in bytes
+               unsigned int     dest  // destination ID
+               ) {
+#pragma HLS INTERFACE m_axi port = in offset = slave bundle = gmem
+#pragma HLS INTERFACE axis port = k2n
+#pragma HLS INTERFACE s_axilite port = in bundle = control
+#pragma HLS INTERFACE s_axilite port = size bundle = control
+#pragma HLS INTERFACE s_axilite port = dest bundle = control
+#pragma HLS INTERFACE s_axilite port = return bundle = control
+
+data_mover:
+
+  unsigned int bytes_per_beat = (DWIDTH / 8);
+  pkt v;
+
+  // Auto-pipeline is going to apply pipeline to this loop
+  for (unsigned int i = 0; i < (size / bytes_per_beat); i++) {
+	#pragma HLS LATENCY min=1 max=1000
+	#pragma HLS PIPELINE II=1
+    ap_uint<512> res = in[i];
+    v.data = res;
+    v.keep = -1;
+    
+    // set last signals when last piece of data or
+    // multiple of 1408 bytes, packetize the payload
+    if ( (((size / bytes_per_beat) - 1)==i) || ((((i + 1) * DWIDTH/8) % 1408) == 0))
+      v.last = 1;
+    else 
+      v.last = 0;
+
+    v.user = dest;
+    k2n.write(v);
+  }
+}
+}
---
+++ kernels/src/krnl_s2mm.cpp
@@ -0,0 +1,77 @@
+/**********
+Copyright (c) 2019, Xilinx, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********/
+/* This is a stream to memory mapped data mover kernel which takes input from a
+stream and writes data
+to global memory via memory mapped interface */
+
+#include "ap_axi_sdata.h"
+#include "ap_int.h"
+#include "hls_stream.h"
+
+#define DWIDTH 512
+#define UWIDTH 16
+
+typedef ap_axiu<DWIDTH, UWIDTH, 1, 1> pkt;
+
+extern "C" {
+void krnl_s2mm(ap_uint<DWIDTH> *out,     // Write only memory mapped
+               hls::stream<pkt> &n2k,    // Internal Stream
+               unsigned int     size     // Size in bytes
+               ) {
+#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem
+#pragma HLS INTERFACE axis port = n2k
+#pragma HLS INTERFACE s_axilite port = out bundle = control
+#pragma HLS INTERFACE s_axilite port = size bundle = control
+#pragma HLS INTERFACE s_axilite port = return bundle = control
+
+  unsigned int bytes_per_beat = (DWIDTH / 8);
+  //unsigned int count = 0:
+data_mover:
+
+  int i = 0;
+  pkt v;
+  // Auto-pipeline is going to apply pipeline to this loop
+  for (unsigned int i = 0; i < (size / bytes_per_beat); i++) {
+    n2k.read(v);
+    out[i] = v.data;
+  }
+  // do {
+  //   n2k.read(v);
+  //   out[i] = v.data;
+  //   count++;
+  // } while (v.last != 1)
+  // size = count;
+}
+}

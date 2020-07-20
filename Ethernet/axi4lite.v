b919e185ad68ccf07ce7e5ca772215a848650d3b
Mario Ruiz <mruiznog@xilinx.com>
2020-07-20 19:11:48 +0100

    Missing file to provide link debug

---
+++ Ethernet/axi4lite.v
@@ -0,0 +1,371 @@
+/* (c) Copyright 2020 Xilinx, Inc. All rights reserved.
+ This file contains confidential and proprietary information 
+ of Xilinx, Inc. and is protected under U.S. and
+ international copyright and other intellectual property 
+ laws.
+ 
+ DISCLAIMER
+ This disclaimer is not a license and does not grant any 
+ rights to the materials distributed herewith. Except as 
+ otherwise provided in a valid license issued to you by 
+ Xilinx, and to the maximum extent permitted by applicable
+ law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+ WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES 
+ AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING 
+ BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+ INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and 
+ (2) Xilinx shall not be liable (whether in contract or tort, 
+ including negligence, or under any other theory of 
+ liability) for any loss or damage of any kind or nature 
+ related to, arising under or in connection with these 
+ materials, including for any direct, or any indirect, 
+ special, incidental, or consequential loss or damage 
+ (including loss of data, profits, goodwill, or any type of 
+ loss or damage suffered as a result of any action brought 
+ by a third party) even if such damage or loss was 
+ reasonably foreseeable or Xilinx had been advised of the 
+ possibility of the same.
+ 
+ CRITICAL APPLICATIONS
+ Xilinx products are not designed or intended to be fail-
+ safe, or for use in any application requiring fail-safe
+ performance, such as life-support or safety devices or 
+ systems, Class III medical devices, nuclear facilities, 
+ applications related to the deployment of airbags, or any 
+ other applications that could lead to death, personal 
+ injury, or severe property or environmental damage 
+ (individually and collectively, "Critical 
+ Applications"). Customer assumes the sole risk and 
+ liability of any use of Xilinx products in Critical 
+ Applications, subject only to applicable laws and 
+ regulations governing limitations on product liability.
+ 
+ THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS 
+ PART OF THIS FILE AT ALL TIMES.
+*/
+
+
+`timescale 1 ns / 1 ps
+`default_nettype wire
+
+module axi4lite # (
+    // Width of S_AXIL data bus
+    parameter integer AXIL_DATA_WIDTH    = 32,
+    // Width of S_AXIL address bus
+    parameter integer AXIL_ADDR_WIDTH    =  4,
+    // Width of TDEST address bus
+    parameter integer STREAMING_TDEST_WIDTH = 16
+
+)
+(
+    // Global Clock Signal
+    input wire                                  S_AXIL_ACLK,
+    // Global Reset Signal. This Signal is Active LOW
+    input wire                                  S_AXIL_ARESETN,
+    // Write address (issued by master, accepted by Slave)
+    input wire [AXIL_ADDR_WIDTH-1 : 0]          S_AXIL_AWADDR,
+    // Write address valid. This signal indicates that the master signaling
+        // valid write address and control information.
+    input wire                                  S_AXIL_AWVALID,
+    // Write address ready. This signal indicates that the slave is ready
+        // to accept an address and associated control signals.
+    output wire                                 S_AXIL_AWREADY,
+    // Write data (issued by master, accepted by Slave) 
+    input wire [AXIL_DATA_WIDTH-1 : 0]          S_AXIL_WDATA,
+    // Write strobes. This signal indicates which byte lanes hold
+        // valid data. There is one write strobe bit for each eight
+        // bits of the write data bus.    
+    input wire [(AXIL_DATA_WIDTH/8)-1 : 0]      S_AXIL_WSTRB,
+    // Write valid. This signal indicates that valid write
+        // data and strobes are available.
+    input wire                                  S_AXIL_WVALID,
+    // Write ready. This signal indicates that the slave
+        // can accept the write data.
+    output wire                                 S_AXIL_WREADY,
+    // Write response. This signal indicates the status
+        // of the write transaction.
+    output wire [1 : 0]                         S_AXIL_BRESP,
+    // Write response valid. This signal indicates that the channel
+        // is signaling a valid write response.
+    output wire                                 S_AXIL_BVALID,
+    // Response ready. This signal indicates that the master
+        // can accept a write response.
+    input wire                                  S_AXIL_BREADY,
+    // Read address (issued by master, accepted by Slave)
+    input wire [AXIL_ADDR_WIDTH-1 : 0]          S_AXIL_ARADDR,
+    // Read address valid. This signal indicates that the channel
+        // is signaling valid read address and control information.
+    input wire                                  S_AXIL_ARVALID,
+    // Read address ready. This signal indicates that the slave is
+        // ready to accept an address and associated control signals.
+    output wire                                 S_AXIL_ARREADY,
+    // Read data (issued by slave)
+    output wire [AXIL_DATA_WIDTH-1 : 0]         S_AXIL_RDATA,
+    // Read response. This signal indicates the status of the
+        // read transfer.
+    output wire [1 : 0]                         S_AXIL_RRESP,
+    // Read valid. This signal indicates that the channel is
+        // signaling the required read data.
+    output wire                                 S_AXIL_RVALID,
+    // Read ready. This signal indicates that the master can
+        // accept the read data and response information.
+    input wire                                  S_AXIL_RREADY,
+
+    // CMAC status
+    input wire                                  tx_done_led,
+    input wire                                  tx_busy_led,
+    input wire                                  rx_gt_locked_led,
+    input wire                                  rx_aligned_led,
+    input wire                                  rx_done_led,
+    input wire                                  rx_data_fail_led,
+    input wire                                  rx_busy_led,
+    input wire                                  cmac_aligned_sync
+
+);
+
+    // AXI4LITE signals
+    reg [AXIL_ADDR_WIDTH-1 : 0]  axi_awaddr;
+    reg     axi_awready;
+    reg     axi_wready;
+    reg [1 : 0]     axi_bresp;
+    reg     axi_bvalid;
+    reg [AXIL_ADDR_WIDTH-1 : 0]  axi_araddr;
+    reg     axi_arready;
+    reg [AXIL_DATA_WIDTH-1 : 0]  axi_rdata;
+    reg [1 : 0]     axi_rresp;
+    reg     axi_rvalid;
+
+    // Example-specific design signals
+    // local parameter for addressing 32 bit / 64 bit AXIL_DATA_WIDTH
+    // ADDR_LSB is used for addressing 32/64 bit registers/memories
+    // ADDR_LSB = 2 for 32 bits (n downto 2)
+    // ADDR_LSB = 3 for 64 bits (n downto 3)
+    localparam integer ADDR_LSB = (AXIL_DATA_WIDTH/32) + 1;
+    localparam integer OPT_MEM_ADDR_BITS = 1;
+    //----------------------------------------------
+    //-- Signals for user logic register space example
+    //------------------------------------------------
+    //-- Number of Slave Registers 32
+    reg [AXIL_DATA_WIDTH-1:0]    dummy;
+    wire     slv_reg_rden;
+    wire     slv_reg_wren;
+    reg [AXIL_DATA_WIDTH-1:0]     reg_data_out;
+    integer  byte_index;
+    reg  aw_en;
+
+    // I/O Connections assignments
+
+    assign S_AXIL_AWREADY    = axi_awready;
+    assign S_AXIL_WREADY     = axi_wready;
+    assign S_AXIL_BRESP      = axi_bresp;
+    assign S_AXIL_BVALID     = axi_bvalid;
+    assign S_AXIL_ARREADY    = axi_arready;
+    assign S_AXIL_RDATA      = axi_rdata;
+    assign S_AXIL_RRESP      = axi_rresp;
+    assign S_AXIL_RVALID     = axi_rvalid;
+
+    // Implement axi_awready generation
+    // axi_awready is asserted for one S_AXIL_ACLK clock cycle when both
+    // S_AXIL_AWVALID and S_AXIL_WVALID are asserted. axi_awready is
+    // de-asserted when reset is low.
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            axi_awready <= 1'b0;
+            aw_en <= 1'b1;
+        end 
+        else begin    
+            if (~axi_awready && S_AXIL_AWVALID && S_AXIL_WVALID && aw_en) begin
+                // slave is ready to accept write address when 
+                // there is a valid write address and write data
+                // on the write address and data bus. This design 
+                // expects no outstanding transactions. 
+                axi_awready <= 1'b1;
+                aw_en <= 1'b0;
+            end
+            else if (S_AXIL_BREADY && axi_bvalid) begin
+                aw_en <= 1'b1;
+                axi_awready <= 1'b0;
+            end
+            else begin
+                axi_awready <= 1'b0;
+            end
+        end 
+    end       
+
+    // Implement axi_awaddr latching
+    // This process is used to latch the address when both 
+    // S_AXIL_AWVALID and S_AXIL_WVALID are valid. 
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            axi_awaddr <= 0;
+        end 
+        else begin    
+            if (~axi_awready && S_AXIL_AWVALID && S_AXIL_WVALID && aw_en) begin
+                // Write Address latching 
+                axi_awaddr <= S_AXIL_AWADDR;
+            end
+        end 
+    end       
+
+    // Implement axi_wready generation
+    // axi_wready is asserted for one S_AXIL_ACLK clock cycle when both
+    // S_AXIL_AWVALID and S_AXIL_WVALID are asserted. axi_wready is 
+    // de-asserted when reset is low. 
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            axi_wready <= 1'b0;
+        end 
+        else  begin    
+            if (~axi_wready && S_AXIL_WVALID && S_AXIL_AWVALID && aw_en ) begin
+                // slave is ready to accept write data when 
+                // there is a valid write address and write data
+                // on the write address and data bus. This design 
+                // expects no outstanding transactions. 
+                axi_wready <= 1'b1;
+            end
+            else begin
+                axi_wready <= 1'b0;
+            end
+        end 
+    end       
+
+    // Implement memory mapped register select and write logic generation
+    // The write data is accepted and written to memory mapped registers when
+    // axi_awready, S_AXIL_WVALID, axi_wready and S_AXIL_WVALID are asserted. Write strobes are used to
+    // select byte enables of slave registers while writing.
+    // These registers are cleared when reset (active low) is applied.
+    // Slave register write enable is asserted when valid address and data are available
+    // and the slave is ready to accept the write address and write data.
+    assign slv_reg_wren = axi_wready && S_AXIL_WVALID && axi_awready && S_AXIL_AWVALID;
+
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            dummy      <= 32'h0;
+        end 
+        else begin
+            // Self clear
+            if (slv_reg_wren) begin
+                case ( axi_awaddr[ADDR_LSB+OPT_MEM_ADDR_BITS:ADDR_LSB] )
+                    default : begin
+                        dummy      <= dummy;
+                    end
+                endcase
+            end
+        end
+    end    
+
+    // Implement write response logic generation
+    // The write response and response valid signals are asserted by the slave 
+    // when axi_wready, S_AXIL_WVALID, axi_wready and S_AXIL_WVALID are asserted.  
+    // This marks the acceptance of address and indicates the status of 
+    // write transaction.
+
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            axi_bvalid  <= 0;
+            axi_bresp   <= 2'b0;
+        end 
+        else begin    
+            if (axi_awready && S_AXIL_AWVALID && ~axi_bvalid && axi_wready && S_AXIL_WVALID) begin
+                // indicates a valid write response is available
+                axi_bvalid <= 1'b1;
+                axi_bresp  <= 2'b0; // 'OKAY' response 
+            end                   // work error responses in future
+            else begin
+                if (S_AXIL_BREADY && axi_bvalid) begin
+                //check if bready is asserted while bvalid is high) 
+                //(there is a possibility that bready is always asserted high)   
+                  axi_bvalid <= 1'b0; 
+                end
+            end
+        end
+    end   
+
+    // Implement axi_arready generation
+    // axi_arready is asserted for one S_AXIL_ACLK clock cycle when
+    // S_AXIL_ARVALID is asserted. axi_awready is 
+    // de-asserted when reset (active low) is asserted. 
+    // The read address is also latched when S_AXIL_ARVALID is 
+    // asserted. axi_araddr is reset to zero on reset assertion.
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            axi_arready <= 1'b0;
+            axi_araddr  <= 32'b0;
+        end 
+        else begin    
+            if (~axi_arready && S_AXIL_ARVALID) begin
+                // indicates that the slave has acceped the valid read address
+                axi_arready <= 1'b1;
+                // Read address latching
+                axi_araddr  <= S_AXIL_ARADDR;
+            end
+            else begin
+                axi_arready <= 1'b0;
+            end
+        end 
+    end       
+
+    // Implement axi_arvalid generation
+    // axi_rvalid is asserted for one S_AXIL_ACLK clock cycle when both 
+    // S_AXIL_ARVALID and axi_arready are asserted. The slave registers 
+    // data are available on the axi_rdata bus at this instance. The 
+    // assertion of axi_rvalid marks the validity of read data on the 
+    // bus and axi_rresp indicates the status of read transaction.axi_rvalid 
+    // is de-asserted on reset (active low). axi_rresp and axi_rdata are 
+    // cleared to zero on reset (active low).  
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            axi_rvalid <= 0;
+            axi_rresp  <= 0;
+        end 
+        else begin    
+            if (axi_arready && S_AXIL_ARVALID && ~axi_rvalid) begin
+                // Valid read data is available at the read data bus
+                axi_rvalid <= 1'b1;
+                axi_rresp  <= 2'b0; // 'OKAY' response
+            end 
+            else if (axi_rvalid && S_AXIL_RREADY) begin
+                // Read data is accepted by the master
+                axi_rvalid <= 1'b0;
+            end                
+        end
+    end    
+
+    reg ap_done_1d;
+    // Implement memory mapped register select and read logic generation
+    // Slave register read enable is asserted when valid address is available
+    // and the slave is ready to accept the read address.
+    assign slv_reg_rden = axi_arready & S_AXIL_ARVALID & ~axi_rvalid;
+    always @(*) begin
+          // Address decoding for reading registers
+          case ( axi_araddr[ADDR_LSB+OPT_MEM_ADDR_BITS:ADDR_LSB] )
+            2'h00   : reg_data_out <= {
+                        {{3{1'b0}},rx_busy_led},
+                        {{3{1'b0}},rx_data_fail_led},
+                        {{3{1'b0}},rx_done_led},
+                        {{3{1'b0}},tx_busy_led},
+                        {{3{1'b0}},tx_done_led},
+                        {{3{1'b0}},rx_gt_locked_led},
+                        {{3{1'b0}},rx_aligned_led},
+                        {{3{1'b0}},cmac_aligned_sync}
+                    };
+            default : reg_data_out <= {32{1'b0}};
+          endcase
+    end
+
+    // Output register or memory read data
+    always @( posedge S_AXIL_ACLK ) begin
+        if ( S_AXIL_ARESETN == 1'b0 ) begin
+            axi_rdata  <= 0;
+        end 
+        else begin    
+        // When there is a valid read address (S_AXIL_ARVALID) with 
+        // acceptance of read address by the slave (axi_arready), 
+        // output the read dada 
+        if (slv_reg_rden) begin
+            axi_rdata <= reg_data_out;     // register read data
+        end   
+        end
+    end
+
+endmodule

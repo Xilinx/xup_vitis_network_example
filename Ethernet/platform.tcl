47b9c664d7266270618b3bcaa1a48e172a594f05
HongshiTan <sasuke.ths@gmail.com>
2023-08-23 17:57:19 +0800

    Add support for U55N
    * refactor ping_fpga example for a better config of ip address.
    * add dump of arp table in ping_fpga example

--- Ethernet/platform.tcl
+++ Ethernet/platform.tcl
@@ -5,9 +5,11 @@
 set words [split $device "_"]
 set board [lindex $words 1]
 
-if {[string first "u50" ${board}] != -1} {
+if {[string first "u55n" ${board}] != -1} {
+    set projPart "xcu55n-fsvh2892-2L-e"
+} elseif {[string first "u50" ${board}] != -1} {
     set projPart "xcu50-fsvh2104-2L-e"
-} elseif {[string first "u55" ${board}] != -1} {
+} elseif {[string first "u55c" ${board}] != -1} {
     set projPart "xcu55c-fsvh2892-2L-e"
 } elseif {[string first "u200" ${board}] != -1} {
     set projPart "xcu200-fsgd2104-2-e"
@@ -15,6 +17,8 @@ if {[string first "u50" ${board}] != -1} {
     set projPart "xcu250-figd2104-2L-e"
 } elseif {[string first "u280" ${board}] != -1} {
     set projPart "xcu280-fsvh2892-2L-e"
+} elseif {[string first "vck5000" ${board}] != -1} {
+    set projPart "xcvc1902-vsva2197-2MP-e-S"
 } else {
     catch {common::send_gid_msg -ssname BD::TCL -id 2041 -severity "ERROR" "unsupported device: ${device}"}
     return 1
--- README.md
+++ README.md
@@ -115,6 +115,7 @@ To install `pynq` and JupyterLab follow the steps [here](Notebooks/README.md#env
 | U50   | xilinx_u50_gen3x16_xdma_5_202210_1     |       |
 | U55C  | xilinx_u55c_gen3x16_xdma_2_202110_1    |       |
 | U55C  | xilinx_u55c_gen3x16_xdma_3_202210_1    |       |
+| U55N  | xilinx_u55n_gen3x4_xdma_2_202110_1     | [Supported only until 2021.2](https://support.xilinx.com/s/article/33838?language=en_US) |
 | U200  | xilinx_u200_gen3x16_xdma_1_202110_1    |       |
 | U250  | xilinx_u250_gen3x16_xdma_3_1_202020_1  | [DFX two-stage platform](https://support.xilinx.com/s/article/75975?language=en_US) |
 | U250  | xilinx_u250_gen3x16_xdma_4_1_202210_1  | [DFX two-stage platform](https://support.xilinx.com/s/article/75975?language=en_US) |
